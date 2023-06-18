019053aa1aae39098a0a1c400edccdbd1a9bcfb5
Name surname <someonmyway@gmail.com>
2023-06-18 19:06:08 +0300

    Added .gitignore file. Copied all files from public est touch project for future use.

---
+++ EspExtLib/EspExtLib/EspExtLib.h
@@ -0,0 +1,66 @@
+//
+//  EspExtLib.h
+//  EspExtLib
+//
+//  Created by Karim Aliiev on 12.06.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for EspExtLib.
+FOUNDATION_EXPORT double EspExtLibVersionNumber;
+
+//! Project version string for EspExtLib.
+FOUNDATION_EXPORT const unsigned char EspExtLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EspExtLib/PublicHeader.h>
+
+#import "AFCompatibilityMacros.h"
+#import "AFHTTPSessionManager.h"
+#import "AFHTTPSessionManager.h"
+#import "AFNetworking.h"
+#import "AFNetworkReachabilityManager.h"
+#import "AFSecurityPolicy.h"
+#import "AFURLRequestSerialization.h"
+#import "AFURLResponseSerialization.h"
+#import "AFURLSessionManager.h"
+
+#import "CocoaAsyncSocket.h"
+
+#import "DDAbstractDatabaseLogger.h"
+#import "DDASLLogger.h"
+#import "DDContextFilterLogFormatter.h"
+#import "DDDispatchQueueLogFormatter.h"
+#import "DDFileLogger.h"
+#import "DDLog.h"
+#import "DDLog+LOGV.h"
+#import "DDMultiFormatter.h"
+#import "DDTTYLogger.h"
+#import "ESP_ByteUtil.h"
+#import "ESP_CRC8.h"
+#import "ESP_NetUtil.h"
+#import "ESP_WifiUtil.h"
+#import "ESPAES.h"
+#import "ESPDataCode.h"
+#import "ESPDatumCode.h"
+#import "ESPGuideCode.h"
+#import "ESPPacketUtils.h"
+#import "ESPProvisioner.h"
+#import "ESPProvisioningListeners.h"
+#import "ESPProvisioningParams.h"
+#import "ESPProvisioningRequest.h"
+#import "ESPProvisioningResult.h"
+#import "ESPProvisioningUDP.h"
+#import "ESPTools.h"
+#import "ESPTouchDelegate.h"
+#import "ESPTouchGenerator.h"
+#import "EspTouchLib.h"
+#import "ESPTouchResult.h"
+#import "ESPTouchTask.h"
+#import "ESPTouchTaskParameter.h"
+#import "ESPUDPSocketClient.h"
+#import "ESPUDPSocketServer.h"
+#import "ESPVersionMacro.h"
+#import "GCDAsyncSocket.h"
+#import "GCDAsyncUdpSocket.h"
